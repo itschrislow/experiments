c4ba71eb5285b1a66fe36c35ed050abad6fd51de
chris0610 <chrislow360@gmail.com>
2022-03-21 11:16:09 -0400

    add anime4

--- pages/animejs/index.jsx
+++ pages/animejs/index.jsx
@@ -5,6 +5,7 @@ const AnimeJS = () => {
   const anime1Ref = useRef();
   const anime2Ref = useRef();
   const anime3Ref = useRef();
+  const anime4Ref = useRef();
 
   useEffect(() => {
     anime1Ref.current = anime({
@@ -56,6 +57,14 @@ const AnimeJS = () => {
       direction: "alternate",
       loop: true,
     });
+
+    anime4Ref.current = anime({
+      targets: ".anime4 path",
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: "easeInOutCubic",
+      duration: 1000,
+      loop: true,
+    });
   }, []);
 
   return (
@@ -90,6 +99,91 @@ const AnimeJS = () => {
               />
             ))}
         </div>
+      </div>
+      {/* ANIME 4 */}
+      <div className="h-16">
+        <svg
+          ref={anime4Ref}
+          xmlns="http://www.w3.org/2000/svg"
+          width="48px"
+          height="48px"
+          viewBox="0 0 48 48"
+          className="absolute"
+        >
+          <g fill="#000000" className="anime4">
+            <path
+              fill="none"
+              stroke="#000000"
+              strokeLinecap="square"
+              strokeMiterlimit="10"
+              strokeWidth="2"
+              d="M24 10v24"
+            />
+            <circle
+              cx="24"
+              cy="7"
+              r="3"
+              fill="none"
+              stroke="#000000"
+              strokeLinecap="square"
+              strokeMiterlimit="10"
+              strokeWidth="2"
+            />
+            <path
+              d="M17 24a8 8 0 0 1-16 0l8-10z"
+              fill="none"
+              stroke="#000000"
+              strokeLinecap="square"
+              strokeMiterlimit="10"
+              strokeWidth="2"
+            />
+            <path
+              fill="none"
+              stroke="#000000"
+              strokeMiterlimit="10"
+              strokeWidth="2"
+              d="M1 24h16"
+            />
+            <path
+              d="M24 38a6 6 0 0 1 6 6H18a6 6 0 0 1 6-6z"
+              fill="none"
+              stroke="#000000"
+              strokeMiterlimit="10"
+              strokeWidth="2"
+            />
+            <path
+              d="M21.058 7.588l-9.316 1.864a6 6 0 0 1-4.925-1.2L4 6"
+              fill="none"
+              stroke="#000000"
+              strokeLinecap="square"
+              strokeMiterlimit="10"
+              strokeWidth="2"
+            />
+            <path
+              d="M31 24a8 8 0 0 0 16 0l-8-10z"
+              fill="none"
+              stroke="#000000"
+              strokeLinecap="square"
+              strokeMiterlimit="10"
+              strokeWidth="2"
+            />
+            <path
+              fill="none"
+              stroke="#000000"
+              strokeMiterlimit="10"
+              strokeWidth="2"
+              d="M47 24H31"
+            />
+            <path
+              d="M26.942 7.588l9.316 1.864a6 6 0 0 0 4.925-1.2L44 6"
+              fill="none"
+              stroke="#000000"
+              strokeLinecap="square"
+              strokeMiterlimit="10"
+              strokeWidth="2"
+            />
+          </g>
+        </svg>
       </div>
     </div>
   );
